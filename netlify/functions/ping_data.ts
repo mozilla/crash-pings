61f2cd1be2a2952e1544423e0c059f261da5ca4d
Alex Franchuk <afranchuk@mozilla.com>
2025-05-14 13:30:33 -0400

    Harden the ping_data function to only accept date strings.

--- netlify/functions/ping_data.ts
+++ netlify/functions/ping_data.ts
@@ -7,6 +7,10 @@ const CACHE_MAX_AGE: number = 60 * 60 * 24; // 1 day
 export default async (req: Request, context: Context): Promise<Response> => {
 	const { date } = context.params;
 
+	if (!date.match(/\d{4}-\d{2}-\d{2}/)) {
+		return new Response(null, { status: 400 });
+	}
+
 	const store = getStore("ping-data");
 	let etag: string | undefined;
 	{
