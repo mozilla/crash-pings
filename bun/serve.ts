f1697eb4c76bbdedc558a8f2c92ea16ff410bec8
Alex Franchuk <afranchuk@mozilla.com>
2026-03-05 10:23:12 -0500

    Add solidjs router

--- bun/serve.ts
+++ bun/serve.ts
@@ -5,12 +5,14 @@ const watcher = watch('./src', { recursive: true });
 
 const server = Bun.serve({
    port: 3000,
-   fetch(req) {
-      var path = new URL(req.url).pathname;
-      if (path === "/") {
-         path = "/index.html";
+   async fetch(req) {
+      let path = new URL(req.url).pathname;
+      let file = Bun.file(`${build.OUTPUT}${path}`);
+      // Redirect missing paths to the SPA index.
+      if (!await file.exists()) {
+         file = Bun.file(`${build.OUTPUT}/index.html`);
       }
-      return new Response(Bun.file(`${build.OUTPUT}${path}`));
+      return new Response(file);
    }
 });
 console.log(`Serving on port ${server.port}`);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { render } from "solid-js/web";
+import html from "solid-js/html";
+import { Router, Route } from "@solidjs/router";
 import App from "./components/App";
 
-render(App, document.body)
+render(() => html`
+    <${Router}>
+        <${Route} path="/" component=${App} />
+    <//>
+`, document.body)
